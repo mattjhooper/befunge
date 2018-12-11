ecdfdf70f2e01b69a37e2d14ed0636f0383881ef
Matt Hooper <mhooper@scottlogic.com>
2018-12-11 12:52:15 +0000

    Added Instructions_PeriodShould

---
+++ Befunge.Tests/Instructions/Instructions_PeriodShould.cs
@@ -0,0 +1,32 @@
+using Xunit;
+using Befunge.Instructions;
+using Befunge.Runtime;
+using Moq;
+
+namespace Befunge.UnitTests.Instructions
+{
+    public class Instructions_PeriodShould
+    {
+        private readonly IInstruction _sit;
+        private readonly Mock<IBefungeRunTime> _runtime;
+
+        public Instructions_PeriodShould() 
+        {
+            _sit = new Period();
+            _runtime = new Mock<IBefungeRunTime>();
+        }
+
+        [Fact]
+        public void OutputCorrectResult() {
+            // Arrange
+            _runtime.SetupSequence(r => r.RetrieveLastValue()).Returns(1);
+
+            // Act
+            _sit.Execute(_runtime.Object);
+
+            // Assert
+            _runtime.VerifySet(r => r.Output = "1");
+
+        }
+    }
+}
